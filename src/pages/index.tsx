36158098d02bcd57bef44fa3508cb2c7ddeea3ff
Rafael <1rafaelrocha2@gmail.com>
2022-11-08 14:13:43 -0300

    feat: loading feature added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,34 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import LandingHome from "../components/LandingHome";
 import SlidingCarousel from "../components/SlidingCarousel";
 
 export default function Home() {
+  const [mySkills, setMySkills] = useState<any>(null)
+
+  useEffect(() => {
+    fetch('/api/skills')
+    .then(resp => resp.json())
+    .then(setMySkills)
+  }, []);
+
   return (
-    <div className="flex flex-col">
-      <Header />
-      <LandingHome />
-      <SlidingCarousel />
+    <div>
+      {!mySkills ? (
+        <picture className="h-full w-full bg-black">
+          <img
+            src="https://user-images.githubusercontent.com/99758843/185761535-b0fe5cf6-49e2-44f8-bd35-64100d247654.gif"
+            alt="loading"
+            className='h-[200px] w-[200px] m-auto mt-[10%]'
+          />
+        </picture>
+      ) : (
+        <div className="flex flex-col">
+          <Header />
+          <LandingHome />
+          <SlidingCarousel />
+        </div>
+      )}
     </div>
   )
 }
