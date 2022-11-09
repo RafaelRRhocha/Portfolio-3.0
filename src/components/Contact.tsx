c6d5935753405e0fec48a49d0294daa8f00ede8b
Rafael <1rafaelrocha2@gmail.com>
2022-11-09 19:49:23 -0300

    fix: contact forms

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,11 @@
-import type { FC } from 'react';
-import { useState } from 'react';
+import { FC } from 'react';
 
 interface ContactProps {}
 
 const Contact: FC<ContactProps> = ({}) => {
-  const [sendRequest, setSendRequest] = useState(false);
-  const [disable, setDisable] = useState(true);
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-
-  const verifyInputEmail = ({ target: { value } }: any) => {
-    const regexValidation = /\S+@\w+\.\w+/i;
-    const finalValidation = regexValidation.test(email);
-    setDisable(finalValidation);
-    setEmail(value);
-  };
-
-  const inputName = ({ target: { value }}: any) => setName(value);
-
   return (
     <div id="contato" className="flex flex-col gap-10 justify-center p-10 items-center w-full h-[600px] text-zinc-100 border-b-[0.5px] border-solid border-white">
-      <h1 className="text-[45px] font-semibold tracking-wide">Entre em Contato Comigo!</h1>
+      <h1 className="text-[45px] font-semibold tracking-wide">Entre em Contato!</h1>
       <div className="flex gap-[100px]">
         <div className="flex flex-col gap-4 justify-center items-center">
           <p className="text-[20px] text-center">
@@ -35,47 +20,34 @@ const Contact: FC<ContactProps> = ({}) => {
             Veja meu Currículo!
           </a>
         </div>
-        {!sendRequest ? (
-          <form className="flex flex-col gap-5">
+          <form className="flex flex-col gap-5" action="https://api.staticforms.xyz/submit" method="post">
+            <input type="hidden" name="accessKey" value="cf2d6dd5-6542-475f-ad2d-3c5d473f90a9" />
+            <input type="hidden" name="redirectTo" value="https://rafaelrocha.vercel.app" />
             <input
               type="text"
               className="rounded-lg p-4 bg-[#d4d4d8] w-full max-w-xs text-black placeholder:text-black placeholder:opacity-[0.5]"
               placeholder="email"
               name="email"
-              value={ email }
-              onChange={ verifyInputEmail }
               autoComplete="off"
             />
             <input
               type="text"
               className="rounded-lg p-4 bg-[#d4d4d8] w-full max-w-xs text-black placeholder:text-black placeholder:opacity-[0.5]"
-              placeholder="name"
+              placeholder="nome"
               name="name"
-              value={ name }
-              onChange={ inputName }
             />
             <textarea
               className="rounded-lg p-4 resize-none bg-[#d4d4d8] w-full max-w-xs text-black placeholder:text-black placeholder:opacity-[0.5]"
-              placeholder="message"
-            />
+              placeholder="mensagem"
+              name="message"
+            ></textarea>
             <button
-              disabled={ !disable || email.length < 2 }
-              onClick={() => { setSendRequest(!sendRequest); setEmail('') }}
+              type="submit"
               className="bg-[#00FF7F] rounded-lg p-4 text-black disabled:opacity-[0.5]"
             >
               Send Email
             </button>
           </form>
-        ) : (
-          <div className="flex flex-col gap-2 items-center justify-center">
-            <picture>
-              <img width="80px" height="80px" src="https://img.icons8.com/external-flatart-icons-lineal-color-flatarticons/344/external-success-user-interface-flatart-icons-lineal-color-flatarticons.png" alt="send contact" />
-            </picture>
-            <p>{`${name}, Obrigado pelo Contato!`}</p>
-            <button onClick={() => setSendRequest(!sendRequest)} className="bg-[#00FF7F] rounded-lg p-4 text-black">Enviar Novamente</button>
-          </div>
-        )}
-
       </div>
     </div>
   );
