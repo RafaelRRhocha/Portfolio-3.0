cdec4fb0fdb5af94f2a736baa4710b0bff85cd32
RafaelRRhocha <1rafaelrocha2@gmail.com>
2023-01-21 17:41:51 -0300

    fix: landing home

--- src/components/LandingHome.tsx
+++ src/components/LandingHome.tsx
@@ -7,9 +7,6 @@ const LandingHome: FC<LandingHomeProps> = ({}) => {
     <>
       <div id="home" className="p-[180px] flex justify-center items-center gap-[50px] border-b-[0.5px] border-solid border-white">
         <div className="flex flex-col items-left gap-10 text-zinc-100 text-center">
-          <p className="text-[20px] font-semibold tracking-wide">
-            Olá! Eu me chamo Rafael Rocha, e no momento estou buscando emprego 👋
-          </p>
           <strong className="text-[52px]">
             Desenvolvedor 
             <span className="text-[#FF4A70]"> Full Stack</span> <br />
